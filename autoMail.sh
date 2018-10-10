e512c98674f41e93ee732470e214ce07d9f3ab71
William Wu <williamwu2049@outlook.com>
2018-10-10 15:04:14 -0700

    huge huge update

--- autoMail.sh
+++ autoMail.sh
@@ -36,46 +36,12 @@ mainmenu() {
 sendmail() {
 	echo -n "To: "
 	read recepientEmail
+	isValid=$(validatemail $recepientEmail)
 
-	# quit when recpepient email is empty
-	if [ "$recepientEmail" = "" ]; then
-		echo "Empty email address. Program terminated."
-		exit
-	fi
-
-	tailValid=0
-	validTail=$(echo $recepientEmail | cut -d'@' -f 2)
-	# check length and the validation of the tailing
-	if [ "$(echo -n $validTail | wc -m)" != "$(echo -n $recepientEmail | wc -m)" ]; then
-		# echo "$validTail"
-		COUNT=0
-        while [  $COUNT -lt 5 ]; do
-            if [ "$validTail" = "${VERIFIED_EMAIL[$COUNT]}" ]; then
-				let tailValid=1
-				break
-			fi
-            let COUNT=COUNT+1
-        done
-	fi
-
-	if [ "$tailValid" = "1" ]; then
-		echo -n "Subject: "
-		read subject
-		if ! command_loc="$(type -p "mvim")" || [[ -z $command_loc ]];
-		then
-			vim -v emailBody.txt
-		else
-			mvim -v emailBody.txt
-		fi
-		file="./emailBody.txt"
-		while IFS= read line
-		do
-			bodyBuffer="$bodyBuffer"$'\n'"$line"
-		done <"$file"
-		echo "$bodyBuffer" | mail -s "$subject" "$recepientEmail"
-		rm emailBody.txt
+	if [ "$isValid" = "1" ]; then
+		mailcreate $recepientEmail
 	else
-		echo "Err: Invalid email."
+		echo "Err: \"$recepientEmail\" is an invalid email."
 		exit
 	fi
 
@@ -83,15 +49,37 @@ sendmail() {
 spammail() {
 	echo -n "To: "
 	read recepientEmail
-	if [ "$recepientEmail" = "" ]; then
+	isValid=$(validatemail $recepientEmail)
+
+	if [ "$isValid" = "1" ]; then
+		echo -n "How many email do you want to spam this person: "
+		read spamnum
+		if [ $spamnum -eq $spamnum 2>/dev/null ]; then
+     		if [ "$spamnum" -lt "100" ]; then
+				mailcreate $recepientEmail $spamnum
+			else
+				echo "Hey that's too much spamming! Be nice to people."
+				exit
+			fi
+		else
+    		echo "Err: \"$spamnum\" is not an integer."
+			exit
+		fi
+	else
+		echo "Err: \"$recepientEmail\" is an invalid email."
+		exit
+	fi
+}
+validatemail() {
+	if [ "$1" = "" ]; then
 		echo "Empty email address. Program terminated."
 		exit
 	fi
 
 	tailValid=0
-	validTail=$(echo $recepientEmail | cut -d'@' -f 2)
+	validTail=$(echo $1 | cut -d'@' -f 2)
 	# check length and the validation of the tailing
-	if [ "$(echo -n $validTail | wc -m)" != "$(echo -n $recepientEmail | wc -m)" ]; then
+	if [ "$(echo -n $validTail | wc -m)" != "$(echo -n $1 | wc -m)" ]; then
 		# echo "$validTail"
 		COUNT=0
         while [  $COUNT -lt 5 ]; do
@@ -102,55 +90,47 @@ spammail() {
             let COUNT=COUNT+1
         done
 	fi
-
-	if [ "$tailValid" = "1" ]; then
-		echo -n "Subject: "
-		read subject
-		if ! command_loc="$(type -p "mvim")" || [[ -z $command_loc ]];
-		then
-			vim -v emailBody.txt
-		else
-			mvim -v emailBody.txt
-		fi
+	echo "$tailValid"
+}
+mailcreate() {
+	bodyBuffer=""
+	echo -n "Subject: "
+	read subject
+	if ! command_loc="$(type -p "mvim")" || [[ -z $command_loc ]];
+	then
+		vim -v emailBody.txt
+	else
+		mvim -v emailBody.txt
+	fi
+	if [ -f ./emailBody.txt ]; then
 		file="./emailBody.txt"
 		while IFS= read line
 		do
 			bodyBuffer="$bodyBuffer"$'\n'"$line"
 		done <"$file"
 		rm emailBody.txt
-		echo -n "How many email do you want to spam this person: "
-		read spamnum
-		if [ "$spamnum" -lt "100" ]; then
-			echo -n "Aww why so much hate. Are you sure to spam this person? [y/n]: "
-			read answer
-			if [ "$answer" = "y" ]; then
-				COUNT=0
-				while [ "$COUNT" -lt $spamnum ]; do
-					echo "$bodyBuffer" | mail -s "$subject" "$recepientEmail"
-					let COUNT=COUNT+1
-				done
-			else
-				echo "Thanks for being a nice person!"
-				exit
-			fi
-		elif [ "$spamnum" -ge "100" ]; then
-			echo "Hey that's too much spamming! Be nice to people."
+	fi
+	
+	if [ "$2" = "" ]; then
+		echo "$bodyBuffer" | mail -s "$subject" "$1"
+	else
+		echo -n "Aww why so much hate. Are you sure to spam this person? [y/n]: "
+		read answer
+		if [ "$answer" = "y" ]; then
+			COUNT=0
+			while [ "$COUNT" -lt $2 ]; do
+				echo "$bodyBuffer" | mail -s "$subject" "$1"
+				let COUNT=COUNT+1
+			done
+		elif [ "$answer" = "n" ]; then
+			echo "Thanks for being a nice person!"
 			exit
 		else
-			echo "Invalid input. Program terminated."
+			echo "Err: \"$answer\" is not a valid input."
 			exit
 		fi
-	else
-		echo "Err: Invalid email."
-		exit
 	fi
-}
-subjectCreate() {
-	echo "place holder"
 
-}
-bodyCreate() {
-	echo "another one"
 }
 abortinput() {
 	echo -n "Do you want to continue? [y/n]: "
