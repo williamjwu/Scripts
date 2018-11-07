42929924e5f7693622b00c4b2f341754bac414ff
William Wu <williamwu2049@outlook.com>
2018-11-06 17:34:09 -0800

    pdfread 1.0 !!!

--- pdfread.sh
+++ pdfread.sh
@@ -2,8 +2,12 @@
 
 # if $1 exist and it is true start the program
 # if not then echo out the illegal arguments
-if [ "$1" = "" ]; then
-	echo "hh"
+
+if [ -f ./$1 ]; then
+	pdftotext $1
+	filename=$(echo $1 | cut -d'.' -f 1)
+	vi $filename.txt
+	rm $filename.txt
 else
 	echo "Illegal argument: use flag \"--help\" to look for instructions"
 fi
